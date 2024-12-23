c2082dbf6f878ec294726f56c2c5c4bac20b1d8f
Juan Benitez <juan.a.benitez@gmail.com>
2024-12-23 02:36:40 -0400

    added config file

---
+++ src/esp_gcs_config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Define constants and macros
+#define WIFI_SSID "HOME-WN_A"
+#define WIFI_PASS "admin5678423"
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include <esp_partition.h>
 #include "esp_gcs_system.h"
 #include "esp_gcs_pfd.h"
+#include "my_esp_gcs_config.h"
 
 ESP_GCS_PFD pfd;
 
-void setup() {
-    //esp_gcs_config_t config {"SSID", "PASSWORD", IPAddress(10,0,0,100), TCP, 3000, PARALLEL_9488, 480, 320};
-    esp_gcs_config_t config {"HOME-WN_A", "admin5678423", IPAddress(10,0,0,95), TCP, 3000, PARALLEL_9488, 480, 320};
+void setup() {    
+    esp_gcs_config_t config {WIFI_SSID, WIFI_PASS, IPAddress(10,0,0,95), TCP, 3000, PARALLEL_9488, 480, 320};
     pfd.init( &config );
 }
 
